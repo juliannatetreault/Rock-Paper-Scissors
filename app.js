2d1fea97b1ed2008b119732a7a90a00817932e1e
github username <github email address>
2019-05-06 17:00:37 -0600

    began writing cases for potential game outcomes

--- app.js
+++ app.js
@@ -7,3 +7,60 @@ const resultDiv = document.querySelector('.result');
 const rockDiv = document.getElementById('R');
 const paperDiv = document.getElementById('P');
 const scissorsDiv = document.getElementById('S');
+
+function getComputerChoice() {
+    const choices = ['R', 'P', 'S'];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+}
+
+// getComputerChoice();
+
+function win() {
+}
+
+function lose() {
+}
+
+function draw() {
+}
+
+function game(userChoice) {
+    const computerChoice = getComputerChoice();
+
+    switch (userChoice + computerChoice) {
+        case "RS":
+        case "PR":
+        case "SP":
+          win();
+          break;
+        case "RP":
+        case "PS":
+        case "SR":
+          lose();
+          break;
+        case "RR":
+        case "PP":
+        case "SS":
+          draw();
+          break;
+    }
+}
+
+// game();
+
+function main() {
+  rockDiv.addEventListener('click', function() {
+      game('R');
+  });
+
+  paperDiv.addEventListener('click', function() {
+      game('P');
+  });
+
+  scissorsDiv.addEventListener('click', function() {
+      game('S');
+  });
+}
+
+main();
